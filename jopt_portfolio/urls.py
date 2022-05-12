0b5cb29c1a8cacd3f18e7fc53e43abd7a320db13
Jesus Puentes <jopt98@gmail.com>
2022-05-12 16:54:41 -0500

    Added a keep for production

--- jopt_portfolio/urls.py
+++ jopt_portfolio/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/services/', include('service.urls')),
     path('api/projects/', include('project.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
