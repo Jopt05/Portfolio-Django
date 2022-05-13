63967be492fc047e0749e2fe27a88c91180714a9
Jesus Puentes <jopt98@gmail.com>
2022-05-12 19:06:17 -0500

    Solved some problems

--- jopt_portfolio/settings/base.py
+++ jopt_portfolio/settings/base.py
@@ -46,6 +46,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'jopt_portfolio.urls'
--- jopt_portfolio/settings/local.py
+++ jopt_portfolio/settings/local.py
@@ -3,7 +3,8 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
