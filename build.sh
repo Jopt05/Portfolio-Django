b670ccfd3a4c00605eb098e0bae3b0b410589d5b
Jesus Puentes <jopt98@gmail.com>
2022-09-09 09:32:22 -0500

    Added for superuser

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input --settings=jopt_portfolio.settings.prod
+python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+py manage.py createsuperuser --noinput
