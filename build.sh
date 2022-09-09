e123b44560ee8c1fdadb8053b1c49dcf0536af72
Jesus Puentes <jopt98@gmail.com>
2022-09-09 08:55:30 -0500

    Changed settings root

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --settings=jopt_portfolio.settings.prod
 
 python manage.py migrate
