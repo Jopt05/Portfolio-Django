4a44bc621f854c79d6f0461e2f1c37fb7b3a448a
Jesus Puentes <jopt98@gmail.com>
2022-09-09 09:53:30 -0500

    Changed py for python

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-python manage.py createsuperuser --username $DJANGO_SUPERUSER_USER --email $DJANGO_SUPERUSER_EMAIL --password $DJANGO_SUPERUSER_PASSWORD  --noinput
+python manage.py createsuperuser --username $DJANGO_SUPERUSER_USER --email $DJANGO_SUPERUSER_EMAIL --noinput
