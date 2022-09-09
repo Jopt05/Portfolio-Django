3fd185477c26d651471e9c7e37e5a87ee0417117
Jesus Puentes <jopt98@gmail.com>
2022-09-09 15:32:57 -0500

    Handled smtp exception

--- emailapp/views.py
+++ emailapp/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from django.core.mail import send_mail
 from rest_framework import status
 from decouple import config
+from smtplib import SMTPException
 
 from .serializers import SendEmailSerializer
 
@@ -16,35 +17,33 @@ class EmailView(APIView):
 
     def post(self, request):
         serializer = SendEmailSerializer(data=request.data)
+        error_message = ''
 
         if (serializer.is_valid()):
             name = serializer.validated_data["name"]
             email = serializer.validated_data["email"]
             message = serializer.validated_data["message"]
 
-            was_sended = send_mail(
-                subject=f"New mail from {name} - {email}",
-                message=message,
-                from_email=from_email,
-                recipient_list=[to_email],
-                fail_silently=False,
-            )
+            try:
 
-            if was_sended:
+                was_sended = send_mail(
+                    subject=f"New mail from {name} - {email}",
+                    message=message,
+                    from_email=from_email,
+                    recipient_list=[to_email],
+                    fail_silently=False,
+                )
+
+            except SMTPException as e:
+                print(e)
                 return Response(
                     {
-                        "ok": True
+                        "ok": False
                     },
                     status=status.HTTP_200_OK
                 )
-            else:
-                return Response(
-                    {
-                        "ok": False,
-                        "message": str(e)
-                    },
-                    status=status.HTTP_500_INTERNAL_SERVER_ERROR
-                )
+            except:
+                print("Mail Sending Failed!")
 
         else:
             return Response(
