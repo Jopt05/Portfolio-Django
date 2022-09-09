5cbcbc9117b324563f1dbc5b70e959131b8c132d
Jesus Puentes <jopt98@gmail.com>
2022-09-08 23:29:52 -0500

    Please work

--- jopt_portfolio/settings/prod.py
+++ jopt_portfolio/settings/prod.py
@@ -30,7 +30,7 @@ DATABASES = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
